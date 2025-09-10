3a0c2667c9439db4f59333bb5718961447ebc31b
emin8999 <aemin.10899@gmail.com>
2025-09-10 05:51:33 +0400

    Created detailed Privacy & Security Policy (77 sections) + Added footer section to the website + Updated and improved several functions in Store, Login, and AdminLogin modules

--- frontend/assets/js/footer.js
+++ frontend/assets/js/footer.js
@@ -5,6 +5,7 @@
   // Определяем корректный путь к storeLogin.html и другим страницам
   const inDashboard = window.location.pathname.includes("/dashboard/");
   const sellHref = inDashboard ? "./storeLogin.html" : "./dashboard/storeLogin.html";
+  const privacyHref = inDashboard ? "../privacy.html" : "./privacy.html";
 
   mount.innerHTML = `
     <div class="footer">
@@ -14,9 +15,9 @@
         </div>
         <nav class="footer__nav">
           <a class="footer__link" id="sellWithUs" href="${sellHref}">Sell with us</a>
+          <a class="footer__link" id="privacyPolicy" href="${privacyHref}">Privacy and Security Policy</a>
         </nav>
       </div>
     </div>
   `;
 })();
-
